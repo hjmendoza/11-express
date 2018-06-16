c5165b5522b31dd9bf61fe5ce2d0b7e3ef77ad88
Haley Mendoza <mendozahaley@gmail.com>
2018-06-15 19:54:46 -0700

    removed unnecessary comments from app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 'use strict';
 
 import express from 'express';
+import router from './api/api.js';
+
 let app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-// Our modules -- import the api and "use" it as middleware for express
-import router from './api/api.js';
 app.use( router );
 
-// Flag to know if we are up and going
 let isRunning = false;
 
 module.exports = {
@@ -18,7 +17,6 @@ module.exports = {
     if(! isRunning) {
       app.listen(port, (err) => {
         if(err) { throw err; }
-        // Tick the running flag
         isRunning = true;
         console.log('Server is up on port', port);
       });
